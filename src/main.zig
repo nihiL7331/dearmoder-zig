b44d5b2b981b43e9672202aefa4462b1543bc848
nihiL7331 <pujanekp6@gmail.com>
2026-06-01 13:02:04 +0200

    feat: basic `main.zig` structure
    `main.zig` is responsible for reading cli args and reading file data,
    then passes the data to decoder

---
+++ src/main.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+const Io = std.Io;
+
+const decoder = @import("decoder.zig");
+
+pub fn main(init: std.process.Init) !void {
+    const arena = init.arena.allocator();
+    const args = try init.minimal.args.toSlice(arena);
+
+    if (args.len < 2) {
+        std.debug.print("Usage: {s} <path>\n", .{args[0]});
+        return;
+    }
+
+    const file_path = args[1];
+
+    const file_data = try Io.Dir.readFileAlloc(
+        Io.Dir.cwd(),
+        init.io,
+        file_path,
+        arena,
+        Io.Limit.unlimited,
+    );
+
+    decoder.decode(file_data);
+}
