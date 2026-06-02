80688f0be5e58a029dbf83475c8b14c476bb0850
nihiL7331 <pujanekp6@gmail.com>
2026-06-03 01:13:59 +0200

    refactor: automatic test with expected output

--- src/main.zig
+++ src/main.zig
@@ -48,6 +48,25 @@ pub fn main(init: std.process.Init) !void {
 }
 
 test "decode sdt instrs" {
+    var buf: [1024]u8 = undefined;
+    var w: std.Io.Writer = .fixed(&buf);
+
     const test_data = [_]u32{ 0xE5910008, 0xE5232004, 0xE4D54001, 0xE7C76008, 0xE73A910B, 0x158DC020, 0x041E0008, 0xE7C21243, 0xE4B54010, 0xE7676468 };
-    try decoder.decode(&test_data);
+    const expected =
+        \\LDR R0, [R1, #0x8]
+        \\STR R2, [R3, #-0x4]!
+        \\LDRB R4, [R5], #0x1
+        \\STRB R6, [R7, R8]
+        \\LDR R9, [R10, -R11, LSL #2]!
+        \\STRNE R12, [R13, #0x20]
+        \\LDREQ R0, [R14], #-0x8
+        \\STRB R1, [R2, R3, ASR #4]
+        \\LDRT R4, [R5], #0x10
+        \\STRB R6, [R7, -R8, ROR #8]!
+        \\
+    ;
+
+    try decoder.decode(&w, &test_data);
+
+    try std.testing.expectEqualStrings(expected, buf[0..expected.len]);
 }
